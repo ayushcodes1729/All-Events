1373dde5757f52e8698ac65978e9311d5382ff33
Ayush Kumar <ayushtiwari0803@gmail.com>
2024-03-25 23:12:06 +0530

    All code of Cards added for first time

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Navbar from './components/navbar'
+import Cards from './components/cards'
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <Cards/>
+    </>
+  )
+}
+
+export default App
